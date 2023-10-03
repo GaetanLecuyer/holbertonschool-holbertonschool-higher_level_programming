8151c34f0a6eb7c8509dbfa43cf59c0e0bb3884d
GaetanLecuyer <6229@holbertonstudents.com>
2023-10-03 15:06:34 +0200

    add the file

---
+++ javascript-web_jquery/7-script.js
@@ -0,0 +1,7 @@
+// Make an AJAX GET request to the API URL
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
+  // Extract the character name from the response JSON
+  const characterName = data.name;
+  // Display the character name in the HTML <div id="character">
+  $('#character').text(characterName);
+});
